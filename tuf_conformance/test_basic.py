5e4c04644ebfe5b95fed43cacec9451ebc00d836
Samuel Giddins <segiddins@segiddins.me>
2024-04-29 16:01:58 -0700

    Remove added test assertions

--- tuf_conformance/test_basic.py
+++ tuf_conformance/test_basic.py
@@ -31,19 +31,3 @@ def test_basic_init_and_refresh(client: ClientRunner, server: SimulatorServer) -
     # Verify that expected requests were made
     assert repo.metadata_statistics == [('root', 1), ('root', 2), ('timestamp', None), ('snapshot', 1), ('targets', 1)]
     # TODO verify that local metadata cache has the files we expect
-
-    tmpdir = [ [os.path.relpath( os.path.join(parent, file) , client._tempdir.name) for file in files] for (parent, _, files) in os.walk(client._tempdir.name)]
-    for list in tmpdir: list.sort()
-    assert tmpdir == [
-        ['initial_root.json'], 
-        ['metadata/root.json', 'metadata/snapshot.json', 'metadata/targets.json', 'metadata/timestamp.json']
-    ]
-    for file in tmpdir[1]:
-        with open(os.path.join(client._tempdir.name, file), 'rb') as f:
-            assert strip_signature(f.read()) == strip_signature(repo.fetch_metadata(os.path.basename(file).split('.')[0], 1).decode()), f"{file} does not match"
-
-def strip_signature(b: bytes) -> bytes:
-    value = json.loads(b)  # check if it is valid json
-    for sig in value.get("signatures"):
-        sig.pop("sig")
-    return value
