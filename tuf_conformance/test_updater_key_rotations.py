8f87497465012bea72a8b7686fb22a9d185cec3b
Adam Korczynski <adam@adalogics.com>
2024-08-03 16:33:43 +0100

    add test for non-root rotations
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- tuf_conformance/test_updater_key_rotations.py
+++ tuf_conformance/test_updater_key_rotations.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 from tuf_conformance.client_runner import ClientRunner
 from tuf_conformance.simulator_server import SimulatorServer
 from securesystemslib.signer import CryptoSigner
+from typing import Dict
 
 from tuf.api.metadata import Root
 
@@ -86,10 +87,37 @@ root_rotation_cases = {
     ],
 }
 
-ids = root_rotation_cases.keys()
-cases = root_rotation_cases.values()
+non_root_rotation_cases: Dict[str, MdVersion] = {
+    "1-of-1-key-rotation":
+        MdVersion(keys=[2], threshold=1, sigs=[2]),
+    "1-of-1-key-rotation-unused-signatures":
+        MdVersion(keys=[1], threshold=1, sigs=[3, 1, 4]),
+    "1-of-1-key-rotation-fail-not-signed-with-new-key":
+        MdVersion(keys=[2], threshold=1, sigs=[1, 3, 4], res=False),
+    "3-of-5-one-key-signature-wrong-not-signed-with-3-expected-keys":
+        MdVersion(keys=[0, 1, 3, 4, 5], threshold=3, sigs=[0, 2, 4], res=False),
+    "2-of-5-one-key-signature-mising-threshold-not-reached":
+        MdVersion(keys=[0, 1, 3, 4, 5], threshold=3, sigs=[0, 4], res=False),
+    "3-of-5-sign-first-combo":
+        MdVersion(keys=[0, 1, 2, 3, 4], threshold=3, sigs=[0, 2, 4]),
+    "3-of-5-sign-second-combo":
+        MdVersion(keys=[0, 1, 2, 3, 4], threshold=3, sigs=[0, 4, 1]),
+    "3-of-5-sign-third-combo":
+        MdVersion(keys=[0, 1, 2, 3, 4], threshold=3, sigs=[0, 1, 3]),
+    "3-of-5-sign-fourth-combo":
+        MdVersion(keys=[0, 1, 2, 3, 4], threshold=3, sigs=[1, 2, 3]),
+    "3-of-5-sign-fifth-combo":
+        MdVersion(keys=[0, 1, 2, 3, 4], threshold=3, sigs=[2, 3, 4]),
+}
 
-@pytest.mark.parametrize("root_versions", cases, ids=ids)
+rotation_ids = root_rotation_cases.keys()
+rotation_cases = root_rotation_cases.values()
+
+non_rotation_ids = non_root_rotation_cases.keys()
+non_rotation_cases = non_root_rotation_cases.values()
+
+
+@pytest.mark.parametrize("root_versions", rotation_cases, ids=rotation_ids)
 def test_root_rotation(client: ClientRunner,
                        server: SimulatorServer,
                        root_versions: list[MdVersion]) -> None:
@@ -145,3 +173,61 @@ def test_root_rotation(client: ClientRunner,
 
     with open(os.path.join(client.metadata_dir, "root.json"), "rb") as f:
         assert f.read() == expected_local_root
+
+
+@pytest.mark.parametrize("md_version", non_rotation_cases, ids=non_rotation_ids)
+def test_non_root_rotations(client: ClientRunner,
+                            server: SimulatorServer,
+                            md_version: MdVersion) -> None:
+    """Test Updater.refresh() with various sequences of metadata updates
+
+    Each MdVersion in the list describes metadata keys and signatures
+    of a remote metadata version. As an example:
+        MdVersion([1,2,3], 2, [1,2])
+    defines a metadata that contains keys 1, 2 and 3 with threshold 2. The
+    metadata is signed with keys 1 and 2.
+
+    Assert that refresh() result is expected and that local metadata on disk
+    is the expected one after all roots have been loaded from remote using
+    the standard client update workflow.
+    """
+    signers = []
+    for _ in range(10):
+        signer = CryptoSigner.generate_ed25519()
+        signers.append(signer)
+
+    # initialize a simulator with repository content we need
+    init_data, repo = server.new_test(client.test_name)
+    assert client.init_client(init_data) == 0
+
+    roles = ["timestamp", "snapshot", "targets"]
+    for role in roles:
+        # clear role keys, signers
+        repo.root.roles[role].keyids.clear()
+        repo.signers[role].clear()
+
+        repo.root.roles[role].threshold = md_version.threshold
+        for i in md_version.keys:
+            repo.root.add_key(signers[i].public_key, role)
+
+        for i in md_version.sigs:
+            repo.add_signer(role, signers[i])
+
+        repo.root.version += 1
+        repo.publish_root()
+
+        # run client workflow, assert success/failure
+        expected_error = md_version.res
+        if expected_error:
+            assert client.refresh(init_data) == 0
+
+            # Call fetch_metadata to sign metadata with new keys
+            expected_local_md: bytes = repo.fetch_metadata(role)
+            # assert local metadata role is on disk as expected
+            md_path = os.path.join(client.metadata_dir, f"{role}.json")
+            with open(md_path, "rb") as f:
+                data = f.read()
+                assert data == expected_local_md
+        else:
+            # failure expected
+            assert client.refresh(init_data) == 1
